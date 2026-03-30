8c8aadcec6d014b20c7bc31a088681803189fb41
Chamika Dasun <156738794+dasunc23@users.noreply.github.com>
2026-03-30 16:25:37 +0530

    add frontend react project with tailwind

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="flex items-center justify-center h-screen bg-black">
+      <h1 className="text-4xl font-bold text-white">
+        Welcome to frontend with Tailwind!
+      </h1>
+    </div>
+  )
+}
+
+export default App
