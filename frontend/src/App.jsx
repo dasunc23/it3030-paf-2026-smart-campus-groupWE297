c9ba272f0e84cdfed2f531669ba2d6cfeb02c5a8
Chamika Dasun <156738794+dasunc23@users.noreply.github.com>
2026-04-10 21:00:32 +0530

    update main and app.js

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,22 @@
-import './App.css'
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { Layout } from './components/Layout';
+import { TicketList } from './pages/TicketList';
+import { CreateTicket } from './pages/ReportIssue';
+import { TicketDetails } from './pages/TicketDetails';
 
 function App() {
   return (
-    <div className="flex items-center justify-center h-screen bg-black">
-      <h1 className="text-4xl font-bold text-white">
-        Welcome to frontend with Tailwind!
-      </h1>
-    </div>
-  )
+    <Router>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<TicketList />} />
+          <Route path="/report" element={<CreateTicket />} />
+          <Route path="/ticket/:id" element={<TicketDetails />} />
+        </Routes>
+      </Layout>
+    </Router>
+  );
 }
 
-export default App
+export default App;
