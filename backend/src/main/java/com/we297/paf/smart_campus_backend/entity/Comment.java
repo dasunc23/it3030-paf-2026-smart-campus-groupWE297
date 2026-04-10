7868d68abf202fe33f22cbd25da00d63fe0e0df1
Chamika Dasun <156738794+dasunc23@users.noreply.github.com>
2026-04-10 20:52:57 +0530

    update comment.java

--- backend/src/main/java/com/we297/paf/smart_campus_backend/entity/Comment.java
+++ backend/src/main/java/com/we297/paf/smart_campus_backend/entity/Comment.java
@@ -2,12 +2,15 @@ package com.we297.paf.smart_campus_backend.entity;
 
 import jakarta.persistence.*;
 import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import java.time.LocalDateTime;
 
 @Entity
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
 public class Comment {
 
     @Id
@@ -20,5 +23,7 @@ public class Comment {
 
     private String message;
 
+    @CreatedDate
+    @Column(updatable = false)
     private LocalDateTime createdAt;
 }
